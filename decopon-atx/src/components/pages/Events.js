54f3f43a1ff7a89d14aebc08d30fcb950c9a61d4
Tricia <decoponatx@gmail.com>
2023-11-04 01:16:31 -0500

    made the add to calendar more dynamic

--- decopon-atx/src/components/pages/Events.js
+++ decopon-atx/src/components/pages/Events.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import 'add-to-calendar-button-react'; 
 import { Container, Row, Col, Image } from 'react-bootstrap';
 
@@ -8,6 +8,28 @@ const EventCalendar = () => {
     { id: 2, eventDate: 'Saturday December 16, 2023', eventLocation: 'Hill Country Galleria', eventTime:'11a - 7p', date: '2023-12-16', eventPhoto:'../../img/event1.png', googleMap: 'https://maps.app.goo.gl/uWZRdbxofoviGrCt6', eventName: 'Bee Cave Holiday Arts Market', eventWeb:'https://www.hillcountrygalleria.com/event/holiday-arts-market/2145577009', blurb: 'Make sure to swing by our booth for an exciting decoden experience and grab a kit at a special discount!' },
     { id: 3, eventDate: 'Saturday December 23, 2023',eventLocation: 'Hill Country Galleria', eventTime:'11a - 7p', date: '2023-12-23', eventPhoto:'../../img/event2.png', googleMap: 'https://maps.app.goo.gl/uWZRdbxofoviGrCt6' , eventName: 'Bee Cave Holiday Arts Market', eventWeb:'https://www.hillcountrygalleria.com/event/holiday-arts-market/2145577011', blurb: 'Wrap up your festive shopping spree by visiting our booth for the perfect holiday presents.' }
   ];
+
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+  useEffect(() => {
+    // Handler to set state based on window width
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    // Set up event listener for resize events
+    window.addEventListener('resize', handleResize);
+
+    // Call the handler immediately to set the initial state based on current window size
+    handleResize();
+
+    // Clean up event listener when the component is unmounted
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+
 
   return (
     <Container>
@@ -46,6 +68,7 @@ const EventCalendar = () => {
                   buttonStyle="round"
                   iCalFileName="decopon-event"
                   styleLight="--btn-background: #FEA751; --btn-text: #fff; --font: 'SlowRiver', sans-serif;"
+                  {...(isMobile ? { hideTextLabelButton: true } : {})}
                 />
                 </Row>
               </Col>
